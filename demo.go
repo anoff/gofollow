c076ff78f0b61b67a34be9879582c75e118a5f00
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2019-02-25 22:15:48 +0100

    :star2: feat: get basic data needed

--- demo.go
+++ demo.go
@@ -33,15 +33,61 @@ func main() {
 	client := twitter.NewClient(httpClient)
 
 	// Home Timeline
-	homeTimelineParams := &twitter.HomeTimelineParams{
-		Count:     20,
-		TweetMode: "extended",
+	// homeTimelineParams := &twitter.HomeTimelineParams{
+	// 	Count:     20,
+	// 	TweetMode: "extended",
+	// }
+	// tweets, _, _ := client.Timelines.HomeTimeline(homeTimelineParams)
+	// fmt.Println("User's HOME TIMELINE")
+	// for _, t := range tweets {
+	// 	user := t.User
+	// 	if t.RetweetedStatus != nil {
+	// 		user = t.RetweetedStatus.User
+	// 		fmt.Print("RT: ")
+	// 	}
+	// 	followRatio := float64(user.FriendsCount) / float64(user.FollowersCount)
+	// 	fmt.Println(user.ScreenName, followRatio)
+	// }
+
+	// People I follow
+	// friendListParams := &twitter.FriendListParams{
+	// 	ScreenName: "an0xff",
+	// 	Count:      200,
+	// 	Cursor:     0,
+	// }
+	// fmt.Println("Already followed:")
+	// for {
+	// 	friends, _, _ := client.Friends.List(friendListParams)
+	// 	for _, u := range friends.Users {
+	// 		followRatio := float64(u.FriendsCount) / float64(u.FollowersCount)
+	// 		fmt.Println(u.ScreenName, followRatio)
+	// 	}
+	// 	cursor := friends.NextCursor
+	// 	if cursor == 0 {
+	// 		break
+	// 	}
+	// 	friendListParams.Cursor = cursor
+	// }
+
+	// People that already follow me
+	followerListParams := &twitter.FollowerListParams{
+		ScreenName: "an0xff",
+		Count:      200,
+		Cursor:     0,
 	}
-	tweets, _, _ := client.Timelines.HomeTimeline(homeTimelineParams)
-	fmt.Println("User's HOME TIMELINE")
-	for _, t := range tweets {
-		followRatio := float64(t.User.FriendsCount) / float64(t.User.FollowersCount)
-		fmt.Println(t.User.ScreenName, followRatio)
+	fmt.Println("Already following:")
+
+	for {
+		followers, _, _ := client.Followers.List(followerListParams)
+		for _, u := range followers.Users {
+			followRatio := float64(u.FriendsCount) / float64(u.FollowersCount)
+			fmt.Println(u.ScreenName, followRatio)
+		}
+		cursor := followers.NextCursor
+		if cursor == 0 {
+			break
+		}
+		followerListParams.Cursor = cursor
 	}
 
 }
